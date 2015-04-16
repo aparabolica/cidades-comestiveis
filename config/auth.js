4de3f124553d4f028ca9c86103074a7acd390196
Vitor George <vitor.george@gmail.com>
2015-04-16 15:58:37 -0300

    update deprecated method

--- config/auth.js
+++ config/auth.js
@@ -34,28 +34,28 @@ exports.login = function(req, res, next) {
 
 		// Unknown error
 		if (err) {
-			return res.json(400, messaging.error(req.i18n, err));
+			return res.status(400).json(messaging.error(req.i18n, err));
 
 		// Error raised by passport
 		} else if (info && info.message) {
-			res.json(400, messaging.error(info.message));
+			res.status(400).json(messaging.error(info.message));
 
 		// User not found.
 		} else if (!user) {
-			return res.json(403, messaging.error("access_token.local.unauthorized"));
+			return res.status(403).json(messaging.error("access_token.local.unauthorized"));
 		}
 
 		// User needs to finish migration.
 		else if (user.status == 'to_migrate') {
-			return res.json(400, messaging.error("access_token.local.needs_migration"));
+			return res.status(400).json(messaging.error("access_token.local.needs_migration"));
 
 		// User doesn't have a password, because it logged before via Facebook or Google
 		} else if (!user.hashed_password) {
 			mailer.passwordNeeded(user, req.app.locals.settings.general.serverUrl, user.callback_url, function(err){
 				if (err)
-					return res.json(400, messaging.error("access_token.local.error.send_email"));
+					return res.status(400).json(messaging.error("access_token.local.error.send_email"));
 				else
-					return res.json(400, messaging.error("access_token.local.error.need_password"));
+					return res.status(400).json(messaging.error("access_token.local.error.need_password"));
 			});
 
 		// User needs to confirm his email, send another e-mail
@@ -68,9 +68,9 @@ exports.login = function(req, res, next) {
 
 			req.app.locals.mailer.sendEmail('confirm_email', user.email, data, req.i18n, function(err) {
 				if (err)
-					return res.json(400, messaging.error("access_token.local.error.send_email"));
+					return res.status(400).json(messaging.error("access_token.local.error.send_email"));
 				else
-					return res.json(400, messaging.error("access_token.local.error.needs_email_confirmation"));
+					return res.status(400).json(messaging.error("access_token.local.error.needs_email_confirmation"));
 			});
 
 		// Login successful, proceed with token
@@ -89,16 +89,16 @@ exports.logout = function(req, res, next) {
 	if (req.headers.authorization) {
 		var access_token = req.headers.authorization.split(' ')[1];
 		AccessToken.findOne({_id: access_token}, function(err, at){
-			if (err) return res.json(400, err);
-			if (!at) return res.json(400, messaging.error("access_token.logout.error.inexistent_token"));
+			if (err) return res.status(400).json(err);
+			if (!at) return res.status(400).json(messaging.error("access_token.logout.error.inexistent_token"));
 			at.expired = true;
 			at.save(function(err){
-				if (err) return res.json(400, err);
+				if (err) return res.status(400).json(err);
 				else return res.json(messaging.success('access_token.logout.successful'));
 			});
 		});
 	} else {
-		res.json(400, messaging.error('access_token.logout.error.not_logged'));
+		res.status(400).json(messaging.error('access_token.logout.error.not_logged'));
 	}
 
 };
