decff8dde9db1357167c81cb8a3a25f6d83578dd
Miguel Peixe <miguel@cardume.art.br>
2015-04-16 14:53:39 -0300

    add basic frontend file structure, fix shim and try to fix routes

--- app.js
+++ app.js
@@ -2,35 +2,35 @@
  * Module dependencies
  */
 
-var express = require('express')
-var passport = require('passport')
-var env = process.env.NODE_ENV || 'development'
-var config = require('./config/config')[env]
-var mongoose = require('mongoose')
-var fs = require('fs')
+var express = require('express');
+var passport = require('passport');
+var env = process.env.NODE_ENV || 'development';
+var config = require('./config/config')[env];
+var mongoose = require('mongoose');
+var fs = require('fs');
 
-mongoose.connect(config.db)
+mongoose.connect(config.db);
 
 // Bootstrap models
 fs.readdirSync(__dirname + '/models').forEach(function (file) {
-  if (~file.indexOf('.js')) require(__dirname + '/models/' + file)
-})
+  if (~file.indexOf('.js')) require(__dirname + '/models/' + file);
+});
 
 // Bootstrap passport config
-require('./config/passport')(passport, config)
+require('./config/passport')(passport, config);
 
-var app = express()
+var app = express();
 
 // Bootstrap application settings
-require('./config/express')(app, config, passport)
+require('./config/express')(app, config, passport);
 
 // Bootstrap routes
-require('./config/routes')(app, config)
+require('./config/routes')(app, config);
 
 // Start the app by listening on <port>
-var port = process.env.PORT || 3000
-app.listen(port)
-console.log('Express app started on port '+port)
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Express app started on port ' + port);
 
 // Expose app
-module.exports = app
+module.exports = app;
