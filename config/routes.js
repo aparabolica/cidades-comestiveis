94652a301bfd22d9e3592b88e19f33ea31529215
Vitor George <vitor.george@gmail.com>
2015-04-16 15:21:54 -0300

    set route for client app

--- config/routes.js
+++ config/routes.js
@@ -1,16 +1,20 @@
+var apiRoutes = require('express').Router();
 
-var routes = require('../routes');
-var lands = require('../routes/lands');
-
-/**
- * Expose routes
- */
+var auth = require('./auth');
 
 module.exports = function (app, config) {
-  var apiPrefix = config.apiPrefix;
 
-  // Search
-  app.use('/', routes);
-  app.use(apiPrefix + '/lands', lands);
+  // Authorization
+  apiRoutes.post('/login', auth.login);
+  apiRoutes.get('/logout', auth.logout);
+
+  // Set '/api/v1' as base path for API routes
+  app.use(config.apiPrefix, apiRoutes);
+
+  // For all other routes, send client app
+  app.get('/*', function(req, res) {
+  	res.sendFile(config.rootPath + '/public/views/index.html');
+  });
+
 
 }
