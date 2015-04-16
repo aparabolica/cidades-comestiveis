09ccf139cbad3c0030467bd643fba7e4ab94221d
Vitor George <vitor.george@gmail.com>
2015-04-16 15:26:43 -0300

    add helpers to tests

--- test/api/land.js
+++ test/api/land.js
@@ -1,49 +1,78 @@
+/* Module dependencies */
+
 var request = require('supertest');
+var async = require('async');
 var should = require('should');
+
+/* The app */
+
 var app = require('../../app');
 
-/*
-  Config
- */
+/* Helpers */
+
+var expressHelper = require('../helpers/express');
+var clearDb = require('../helpers/clearDb');
+var factories = require('../helpers/factories');
+
+/* Config */
 
 var apiPrefix = 'api/v1';
+var config = require('../../config/config')['test'];
 
-/*
- * Create user1, user2 and admin
- */
-// function createUsers(doneCreateUsers) {
-//   async.series([function(done){
-//       factory.createUser(function(err,usr){
-//         should.not.exist(err);
-//         user1 = usr;
-//         helper.login(user1.email, user1.password, function(token){
-//           user1AccessToken = token;
-//           done();
-//         });
-//       });
-//     }, function(done){
-//       factory.createUser(function(err,usr){
-//         should.not.exist(err);
-//         user2 = usr;
-//         done()
-//       });
-//     }, function(done){
-//       factory.createUser(function(err,usr){
-//         should.not.exist(err);
-//         admin = usr;
-//         admin.role = 'admin';
-//         done()
-//       });
-//   }], doneCreateUsers);
-// }
+/* The tests */
 
 describe('API: Lands', function(){
 
+  before(function (doneBefore) {
+
+    /*
+     * Init database
+     */
+
+    expressHelper.whenReady(function(){
+      clearDb.all(function(err){
+        should.not.exist(err);
+        async.series([createUsers], doneBefore)
+      });
+    });
+
+    /*
+     * Create user1, user2 and admin
+     */
+    function createUsers(doneCreateUsers) {
+      async.series([function(done){
+          factories.createUser(function(err,usr){
+            should.not.exist(err);
+            user1 = usr;
+            expressHelper.login(user1.email, user1.password, function(token){
+              user1AccessToken = token;
+              done();
+            });
+          });
+        }, function(done){
+          factories.createUser(function(err,usr){
+            should.not.exist(err);
+            user2 = usr;
+            done()
+          });
+        }, function(done){
+          factories.createUser(function(err,usr){
+            should.not.exist(err);
+            admin = usr;
+            admin.role = 'admin';
+            done()
+          });
+      }], doneCreateUsers);
+    }
+  });
+
+
+
   /*
-    POST /api/v1/lands
+    POST /api/version/lands
   */
 
-  describe('POST /api/v1/lands', function(){
+  describe('POST /api/version/lands', function(){
     context('not logged in', function(){
       it('should return 401 (Unauthorized)', function(doneIt){
         request(app)
@@ -66,29 +95,29 @@ describe('API: Lands', function(){
   });
 
   /*
-    GET /api/v1/lands/:id
+    GET /api/version/lands/:id
   */
 
-  describe('GET /api/v1/lands/:id', function(){
+  describe('GET /api/version/lands/:id', function(){
     it('return status 200 and object json for valid id');
     it('return 404 for id not found');
   });
 
   /*
-    GET /api/v1/lands
+    GET /api/version/lands
   */
 
-  describe('GET /api/v1/lands', function(){
+  describe('GET /api/version/lands', function(){
     it('return status 200 (OK) and object json for valid id');
     it('return 400 (Bad request)');
   });
 
 
   /*
-    PUT /api/v1/lands/:id
+    PUT /api/version/lands/:id
   */
 
-  describe('PUT /api/v1/lands/:id', function(){
+  describe('PUT /api/version/lands/:id', function(){
     context('not logged in', function(){
       it('should return 401 (Unauthorized)');
     });
@@ -99,4 +128,14 @@ describe('API: Lands', function(){
     });
   });
 
+  /*
+   * After tests, clear database
+   */
+
+  after(function (done) {
+    clearDb.all(function(err){
+      should.not.exist(err);
+      done(err);
+    });
+  });
 })
