1a53d126498b7055d2a753fd556ecf22f3225443
Vitor George <vitor.george@gmail.com>
2015-09-11 13:00:34 -0300

    fix deprecated morgan call

--- config/express.js
+++ config/express.js
@@ -39,7 +39,7 @@ module.exports = function (app, config, passport) {
 
   // Don't log during tests
   // Logging middleware
-  if (env !== 'test') app.use(morgan(log));
+  if (env !== 'test') app.use(morgan('default', log));
 
   // bodyParser should be above methodOverride
   app.use(bodyParser.json({limit: '50mb'}));
